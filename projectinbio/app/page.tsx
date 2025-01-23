27962a57ee18e35eb683634d01045acff8a04d7d
NathSantos <nathsantos2703@gmail.com>
2025-01-23 17:24:18 -0400

    Criação da Seção VideoExplanation

--- projectinbio/app/page.tsx
+++ projectinbio/app/page.tsx
@@ -1,13 +1,14 @@
 import Hero from '@/components/landingPage/hero';
 import Header from '@/components/landingPage/header';
+import VideoExplanation from '@/components/landingPage/videoExplanation';
 
 export default function Home() {
   return (
     <div className='max-w-7xl mx-auto'>
       <Hero />
       <Header />
+      <VideoExplanation />
       {/*
-    <VideoExplanation/>
     <Pricing/>
     <FAQ/> */}
     </div>
