a6351403ca6ea1398f129d48136e061dfcf3a4b0
NathSantos <nathsantos2703@gmail.com>
2025-01-27 11:53:19 -0400

    Criação de link / salvar no bd

---
+++ projectinbio/app/(pages)/criar/createLinkForm.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { createLink } from '@/actions/create-link';
+import { verifyLink } from '@/actions/verify-link';
+import Button from '@/components/ui/button';
+import TextInput from '@/components/ui/textInput';
+import { sanitizeLink } from '@/lib/utils';
+
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, useState, FormEvent } from 'react';
+
+export default function CreateLinkForm() {
+  const router = useRouter();
+
+  const [link, setLink] = useState('');
+  const [error, setError] = useState('');
+
+  function handleLinkChange(e: ChangeEvent<HTMLInputElement>) {
+    setLink(sanitizeLink(e.target.value));
+    setError('');
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (link.length === 0) return setError('Escolha um link primeiro :)');
+
+    const isLinkTaken = await verifyLink(link);
+    if (isLinkTaken) return setError('Desculpe, esse link já está em uso.');
+
+    const isLinkCreated = await createLink(link);
+
+    if (!isLinkCreated)
+      return setError('Erro ao criar o perfil. Tente novamente.');
+
+    router.push(`/${link}`);
+  }
+
+  return (
+    <>
+      <form
+        action=''
+        onSubmit={handleSubmit}
+        className='w-full flex items-center gap-2'
+      >
+        <span>projectinbio.com/</span>
+        <TextInput value={link} onChange={handleLinkChange} />
+        <Button className='w-[126px]'>Criar</Button>
+      </form>
+      <div>
+        <span className='text-accent-pink'>{error}</span>
+      </div>
+    </>
+  );
+}
--- projectinbio/app/(pages)/criar/page.tsx
+++ projectinbio/app/(pages)/criar/page.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/landingPage/header';
-import Button from '@/components/ui/button';
-import TextInput from '@/components/ui/textInput';
+import CreateLinkForm from '@/(pages)/criar/createLinkForm';
+
 import { Rocket } from 'lucide-react';
 
 export default function CriarPage() {
@@ -12,14 +12,7 @@ export default function CriarPage() {
           <h1 className='text-4xl font-bold text-white'>Escolha seu link</h1>
           <Rocket className='size-10' />
         </div>
-        <form action='' className='w-full flex items-center gap-2'>
-          <span>projectinbio.com/</span>
-          <TextInput />
-          <Button className='w-[126px]'>Criar</Button>
-        </form>
-        <div>
-          <span className='text-accent-pink'>Erro de exemplo</span>
-        </div>
+        <CreateLinkForm />
       </div>
     </div>
   );
