b507551f76ba2c06eeb4af37b76fdeb10d60efab
NathSantos <nathsantos2703@gmail.com>
2025-01-27 10:50:36 -0400

    Google config e implementando autenticação via google

--- projectinbio/app/components/landingPage/header.tsx
+++ projectinbio/app/components/landingPage/header.tsx
@@ -1,6 +1,10 @@
+import { manageAuth } from '@/actions/manage-auth';
 import Button from '@/components/ui/button';
+import { auth } from '@/lib/auth';
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
+
   return (
     <div
       className='absolute top-0 left-0 right-0 max-w-7xl mx-auto flex
@@ -11,8 +15,10 @@ export default function Header() {
         <h3 className='text-white text-2xl font-bold'>ProjectInBio</h3>
       </div>
       <div className='flex items-center gap-4'>
-        <Button>Minha Página</Button>
-        <Button>Sair</Button>
+        {session && <Button>Minha Página</Button>}
+        <form action={manageAuth}>
+          <Button>{session ? 'Sair' : 'Login'}</Button>
+        </form>
       </div>
     </div>
   );
