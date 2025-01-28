cad24f51e35512110f0574f1f825c661355348b8
NathSantos <nathsantos2703@gmail.com>
2025-01-28 19:44:18 -0400

    Controle de Usuário

--- projectinbio/app/(pages)/[profileId]/page.tsx
+++ projectinbio/app/(pages)/[profileId]/page.tsx
@@ -8,7 +8,7 @@ import { getDownloadURLFromPath } from '@/lib/firebase';
 import { increaseProfileVisits } from '@/actions/increase-profile-visits';
 
 import Link from 'next/link';
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 
 export default async function ProfilePage({
   params,
@@ -29,20 +29,26 @@ export default async function ProfilePage({
     await increaseProfileVisits(profileId);
   }
 
+  if (isOwner && !session?.user.isSubscribed && !session?.user.isTrial) {
+    redirect(`/${profileId}/upgrade`);
+  }
+
   // TODO: get projects
   // TODO: adicionar page view
   // TODO: se o usuário não estiver mais no trial, não deixar ver o projeto e redirecionar para a página de upgrade
 
   return (
     <div className='relative h-screen flex p-20 overflow-hidden'>
-      <div className='fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-tertiary'>
-        <span>Você está usando a versão trial.</span>
-        <Link href={`/${profileId}/upgrade`}>
-          <button className='text-accent-green font-bold underline'>
-            Faça o upgrade agora!
-          </button>
-        </Link>
-      </div>
+      {session?.user.isTrial && !session.user.isSubscribed && (
+        <div className='fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-tertiary'>
+          <span>Você está usando a versão trial.</span>
+          <Link href={`/${profileId}/upgrade`}>
+            <button className='text-accent-green font-bold'>
+              Faça o upgrade agora!
+            </button>
+          </Link>
+        </div>
+      )}
       <div className='w-1/2 flex justify-center h-min'>
         <UserCard profileData={profileData} isOwner={isOwner} />
       </div>
--- projectinbio/app/lib/auth.ts
+++ projectinbio/app/lib/auth.ts
@@ -1,13 +1,49 @@
-import NextAuth from 'next-auth';
 import { FirestoreAdapter } from '@auth/firebase-adapter';
-import { firebaseCert } from '@/lib/firebase';
+import { db, firebaseCert } from '@/lib/firebase';
+import { TRIAL_DAYS } from '@/lib/config';
+
+import NextAuth, { DefaultSession } from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { Timestamp } from 'firebase-admin/firestore';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      createdAt: number;
+      isTrial: boolean;
+    } & DefaultSession['user'];
+  }
+  interface User {
+    createdAt: number;
+    isTrial?: boolean;
+    isSubscribed?: boolean;
+  }
+}
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: FirestoreAdapter({
     credential: firebaseCert,
   }),
   providers: [Google],
-  events: {},
-  callbacks: {},
+  events: {
+    createUser: async ({ user }) => {
+      if (!user.id) return;
+      await db.collection('users').doc(user.id).update({
+        createdAt: Timestamp.now().toMillis(),
+      });
+    },
+  },
+  callbacks: {
+    session({ session, user }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          isTrial:
+            new Date(user.createdAt).getTime() >
+              new Date().getTime() - 1000 * 60 * 60 * 24 * TRIAL_DAYS || false,
+        },
+      };
+    },
+  },
 });
